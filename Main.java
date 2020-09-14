e6f5eb9293d674d01f6ac53b90238b41a791eb5c
fiscn <fiscn2709@wrdsb.ca>
2020-09-14 20:48:07 +0000

    completed Question A1Q1 or "Tempature" Program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;  
+
 /**
- *
- * @author 
+ * convert the tempature the user types from Celsius to Fahrenheit
+ * @author Neil Fischer 
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //
+    Scanner input = new Scanner(System.in);
+
+    //  Prompt the User to enter a temperature in celsius 
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius:");   
+
+    // take the input and store it in a variable 
+    int C = input.nextInt(); 
+
+    // Create a second variable that will preform the math formula 
+    int IE = (C*9)/5 + 32; 
+
+    // calculate the tempature in Fahrenheit 
+    int F = IE;
+
+    // Tell the user the answer in both Fahrenheit and Celsius 
+    System.out.println(C + "C is the same as " + F + "F"); 
+
+
+
+  
   }
 }
